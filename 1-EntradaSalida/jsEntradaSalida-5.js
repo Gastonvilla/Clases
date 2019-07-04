1bb506cef535165af592a032f2d4e8a85ef3873c
Gastonvilla <52511988+Gastonvilla@users.noreply.github.com>
2019-07-03 22:18:13 -0300

    Add files via upload

---
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -0,0 +1,15 @@
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function mostrar()
+{	
+    var nombre;
+    var edad;
+
+    nombre = document.getElementById("elnombre").value;
+    edad = document.getElementById("laedad").value
+
+    alert(nombre);
+    alert(edad);
+    
+}
+
