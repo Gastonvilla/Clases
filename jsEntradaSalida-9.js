4da9fb6db1919a84ae8e42ee5526778cef4567dd
Gastonvilla <52511988+Gastonvilla@users.noreply.github.com>
2019-07-05 22:06:08 -0300

    Clase 2

---
+++ jsEntradaSalida-9.js
@@ -0,0 +1,17 @@
+/*Debemos lograr tomar el importe por ID ,
+transformarlo a entero (parseInt), luego
+mostrar el importe con un aumento del 10 %
+en el cuadro de texto "RESULTADO".*/
+function mostrarAumento()
+{
+var sueldo;
+
+
+var mostrarAumento;
+    
+sueldo =document.getElementById("sueldo").value;
+sueldo =parseInt(sueldo);
+mostrarAumento =sueldo +sueldo*10/100;
+document.getElementById ("resultado").value =mostrarAumento;
+
+}
